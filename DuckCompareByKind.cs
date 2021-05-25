c2165b78c7384dbb9f0a9db56fabdbc88bfd283c
Corey Crumpton <31524125+cecrumpton@users.noreply.github.com>
2021-05-25 15:21:00 -0500

    Add files via upload

---
+++ DuckCompareByKind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_Pg369_DuckApp
+{
+    class DuckCompareByKind : IComparer<Duck>
+    {
+        public int Compare(Duck x, Duck y)
+        {
+            if (x.Kind < y.Kind)
+                return -1;
+            else if (x.Kind > y.Kind)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_Pg369_DuckApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>() {
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 17 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 18 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 11 },
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 14 },
+                new Duck() { Kind = KindOfDuck.Decoy, Size = 13 },
+            };
+
+            PrintDucks(ducks);
+            ducks.Sort();
+            PrintDucks(ducks);
+
+            DuckCompareBySize sizeComparer = new DuckCompareBySize();
+            ducks.Sort(sizeComparer);
+            PrintDucks(ducks);
+
+            DuckCompareByKind kindComparer = new DuckCompareByKind();
+            ducks.Sort(kindComparer);
+            PrintDucks(ducks);
+
+            DuckComparer comparer = new DuckComparer();
+
+            comparer.SortBy = SortCriteria.KindThenSize;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+
+            comparer.SortBy = SortCriteria.SizeThenKind;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+
+            Console.ReadKey();
+        }
+
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck);
+            Console.WriteLine("End of List\n");
+        }
+    }
+}
